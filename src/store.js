e547368c70e5ff1085086655af042a00288d4767
FukeKazki <kazkichi0906@gmail.com>
2019-06-22 17:04:40 +0900

    Add: 仮stateの追加

--- src/store.js
+++ src/store.js
@@ -5,7 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    task: [
+      {
+        id: 1,
+        name: 'ハッカソン',
+        end: new Date(2019, 5, 22),
+        difficulty: 1,
+        conditions: ['福岡'],
+      },
+      {
+        id: 2,
+        name: 'レポート',
+        end: new Date(2019, 5, 25),
+        difficulty: 2,
+        conditions: ['北九州高専', '嫌い'],
+      },
+      {
+        id: 3,
+        name: 'テスト勉強',
+        end: new Date(2019, 6, 30),
+        difficulty: 3,
+        conditions: ['北九州高専', 'やりたくない', '時間かかる']
+      },
+    ],
   },
   mutations: {
 
