23c2f1602ca2416891b9b0b64514ee7b3d84cb87
FukeKazki <kazkichi0906@gmail.com>
2019-06-22 17:36:05 +0900

    Update: タスクの表示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="home">
-    <h1>トップページ</h1>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1></h1>
+    <ul>
+      <li v-for="task in $store.state.task" :key="task.id">
+        <p>タスク名: {{ task.name }}</p>
+        <p>期限: {{ task.end }}</p>
+        <p>難易度: {{ task.difficulty }}</p>
+        <p>コンディション: {{ task.conditions }}</p>
+      </li>
+    </ul>
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
@@ -17,3 +25,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  li {
+    list-style: none;
+    border: 1px solid #4d4d4d;
+    margin-top: 1em;
+  }
+</style>
